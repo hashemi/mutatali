207c3c45bd576a9cb34c2a219dd9b7263de5d937
Ahmad Alhashemi <hashemi@users.noreply.github.com>
2017-03-22 22:30:38 -0500

    Initial Commit

---
+++ mutatali/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  mutatali
+//
+//  Created by Ahmad Alhashemi on 2017-03-22.
+//  Copyright © 2017 Ahmad Alhashemi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
